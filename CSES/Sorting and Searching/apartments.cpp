0c3b91b9bb494f7c609004ee4baad398b60d24ea
Foysal-Munsy <bdfoysal555@gmail.com>
2026-01-14 13:04:16 +0600

    [cses - sorting & searching] apartment WA.

---
+++ CSES/Sorting and Searching/apartments.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Foysal Munsy on 14 / 01 / 2026
+// Time: 12 : 53 : 38
+//
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int N = 2e5 + 5;
+
+void solve()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    int i = 0, j = 0, count = 0;
+    while (i < n && j < m)
+    {
+        if (b[j] < (a[i] - k))
+            j++;
+        else if (b[j] > (a[i] + k))
+            i++;
+        else
+            count++, i++, j++;
+    }
+    cout << count << '\n';
+}
+
+signed main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    ll cs = 1;
+    ll t = 1;
+    // cin >> t;
+    for (; t--;)
+    {
+        // cout << "#case " << cs << " :\n";
+        solve();
+        cs++;
+    }
+    return 0;
+}
